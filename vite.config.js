0138f782c79dbf87f3dbf9c57ed6a170b064d306
Tomás Mollinedo <mollinedotomas@gmail.com>
2025-10-02 13:03:30 -0300

    Cambiando base para deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tp2-leng4-TomasMollinedo/'
+  base: '/tp4-leng4-TomasMollinedo/'
 })
